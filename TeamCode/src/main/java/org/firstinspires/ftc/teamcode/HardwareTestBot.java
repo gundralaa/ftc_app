3c7f111e6e8098ad53fca02e550e3d0ca191a24e
gundralaa <mathgame99@gmail.com>
2016-09-28 20:40:51 -0700

    New OpMode Register

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTestBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTestBot.java
@@ -1,7 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 /**
  * Created by abhin on 9/28/2016.
  */
 public class HardwareTestBot {
+
+    //Define all motors set to null
+    public DcMotor FrontRight = null;
+    public DcMotor FrontLeft = null;
+    public DcMotor BackRight = null;
+    public DcMotor BackLeft = null;
+    public DcMotor MotorA = null;
+    public DcMotor MotorB = null;
+
+    //Define local hmap
+    HardwareMap lhmap = null;
+
+
+    public HardwareTestBot() {
+
+    }
+
+    public void init(HardwareMap hmap){
+
+        //Reference the local version of hardware map to argument
+        lhmap = hmap;
+
+        //Define Motors
+        FrontRight = lhmap.dcMotor.get("FrontRight");
+        FrontLeft = lhmap.dcMotor.get("FrontLeft");
+        BackLeft = lhmap.dcMotor.get("BackLeft");
+        BackRight = lhmap.dcMotor.get("BackRight");
+
+        //Set Direction (Might be the other side based on orientation)
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+        //Initialize Power
+        BackRight.setPower(0);
+        BackLeft.setPower(0);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+
+        //Set Run Mode (Change for Encoder Fucntion)
+        BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpMode.java
@@ -1,7 +1,54 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by abhin on 9/28/2016.
  */
-public class TestingOpMode {
+@TeleOp(name = "TestBotOpMode", group = "Testing")
+
+public class TestingOpMode extends OpMode {
+        //Create Hardware Object
+        HardwareTestBot TestBot = new HardwareTestBot();
+
+
+        public void init() {
+            //Initialize all hardware through method
+            TestBot.init(hardwareMap);
+
+        }
+
+        public void loop() {
+            // Get Joystick values
+            double leftValue = -gamepad1.left_stick_y;
+            double rightValue = -gamepad1.right_stick_y;
+
+            //Set power of Drive Motors
+            TestBot.FrontRight.setPower(rightValue);
+            TestBot.BackRight.setPower(rightValue);
+            TestBot.FrontLeft.setPower(leftValue);
+            TestBot.BackLeft.setPower(leftValue);
+
+            //Set Power of Peripherals
+            if(gamepad1.a) {
+                TestBot.MotorA.setPower(1.0);
+            }
+            if(gamepad1.x){
+                TestBot.MotorA.setPower(-1.0);
+            }
+            if (gamepad1.y){
+                TestBot.MotorB.setPower(1.0);
+            }
+            if(gamepad1.b) {
+                TestBot.MotorB.setPower(-1.0);
+            }
+        }
+
 }
